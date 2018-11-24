5459758a81f54041767ecb2009739e1dd0ddb79b
Bogdan Crisan <bogdan.18rap@gmail.com>
2018-11-24 15:39:06 +0200

    Exemplu

--- pwj.py
+++ pwj.py
@@ -2,7 +2,12 @@ import json
 
 
 def handle_json():
-    print("Handle json Here")
+    # print("Handle json Here")
+    json_data = open("repo.json").read()
+    data = json.loads(json_data)
+    for key in data["Github"]:
+        print("Numele este: " + data["Github"][key]["name"] + " si URL-ul este: " + data["Github"][key]["url"])
+        # print(data["Github"][key]["url"])
 
 
 if __name__ == "__main__":
