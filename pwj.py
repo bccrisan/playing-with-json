69a6c3dce0278811bee7cbfbde645486de7a526f
Bogdan Crisan <bogdan.18rap@gmail.com>
2018-11-26 02:34:41 +0200

    Commit

--- pwj.py
+++ pwj.py
@@ -6,9 +6,11 @@ def handle_json():
     json_data = open("repo.json").read()
     data = json.loads(json_data)
     for key in data["Github"]:
-        print("Numele este: " + data["Github"][key]["name"] + " si URL-ul este: " + data["Github"][key]["url"])
+        print("Numele este:\t" + data["Github"][key]["name"] + " si URL-ul este: " + data["Github"][key]["url"])
         # print(data["Github"][key]["url"])
 
 
 if __name__ == "__main__":
     handle_json()
+
+
